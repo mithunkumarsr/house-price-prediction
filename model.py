9e751578c4390abc9abe1fd6b22a5cf6ed7b4b4b
Mithun Kumar S R <mithunkumar.sr@uber.com>
2020-08-08 18:44:09 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,17 +4,24 @@ from sklearn import linear_model
 import pickle
 
 df = pd.read_csv(
-    '/Users/mithunkumar/Desktop/HomePrices/homepricesMultivariate.csv')
+    '/Users/mithunkumar/Desktop/HomePrices/newhousing.csv')
 
-df.bedrooms = df.bedrooms.fillna(df.bedrooms.median())
+#df.bedrooms = df.bedrooms.fillna(df.bedrooms.median())
 
 reg = linear_model.LinearRegression()
-reg.fit(df.drop('price', axis='columns'), df.price)
-#print(reg.predict([[3000, 3, 40]]))
+
+# dropping the irrelevant columns in the multivariate dataset
+reg.fit(df.drop(['price', 'hotwaterheating',
+                 'airconditioning', 'prefarea', 'mainroad', 'semi-furnished', 'unfurnished'], axis='columns'), df.price)
+
+# predicting with area, bedroom, bathrooms, 'basement', stories, guestroom, parking, areaperbedroom, bbratio
+print(reg.predict(
+    [[5500, 3, 2, 2, 1, 0, 1, 1833.22, 0.667]]))
 
 # Saving model to disk
 pickle.dump(reg, open('/Users/mithunkumar/Desktop/HomePrices/model.pkl', 'wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl', 'rb'))
-print(model.predict([[15, 3000, 4]]))
+print(model.predict([[5500, 3, 2, 2, 1, 0, 1, 1833.22, 0.667]]))
+
